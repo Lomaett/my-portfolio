cdccf95c11a0949e9ca203de04dc83641d9dace0
Ajirioghene Oguh <ajirioguh@gmail.com>
2023-12-11 01:09:52 +0100

    Initialize repo

---
+++ src/data.js
@@ -0,0 +1,79 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: "Melbourne Housing Price",
+    subtitle: "Python",
+    description:
+      "Exploratory data analysis and prediction of the cost of purchasing a house in Melbourne City, Australia.",
+    image: "./melbourne.gif",
+    link: "https://github.com/Lomaett/Melbourne_Housing",
+  },
+  {
+    title: "Movie Recommender",
+    subtitle: "Python and Streamlit",
+    description:
+      "Movie recommender system based on both the content-based and collaborative filtering methods.",
+    image: "./movie_recommender.gif",
+    link: "https://github.com/2301ACDSTeamBM4-Unsupervised/unsupervised-predict-streamlit-template",
+  },
+  {
+    title: "Superstore",
+    subtitle: "Tableau",
+    description:
+      "Analysis of United States superstore sales, giving key insights into the categories with the highest sales based on customers' choices as well the locations with highest purchases.",
+    image: "./superstore.gif",
+    link: "https://public.tableau.com/app/profile/ajirioghene.oguh/viz/EDASparks/Dashboard2",
+  },
+  {
+    title: "COVID19",
+    subtitle: "Tableau",
+    description:
+      "Analysis of the global COVID cases between Feb 2020 and May 2022. Key insights includes the number of critical cases and deaths, and also the monthly averages of daily new cases.",
+    image: "./covid.gif",
+    link: "https://public.tableau.com/app/profile/ajirioghene.oguh/viz/COVIDCases2020-2022/Dashboard1",
+  },
+  {
+    title: "Sentiment Analysis",
+    subtitle: "Python and Streamlit",
+    description:
+      "Machine Learning model that is able to classify whether or not a person believes in climate change based on their tweets.",
+    image: "./tweet.gif",
+    link: "https://github.com/2301ACDSES1-Classification/Advanced_Classification",
+  },
+  {
+    title: "Mobile Signal strength",
+    subtitle: "Python, Keplergl and Streamlit",
+    description:
+      "Analysis of mobile signal strength; location and optimisation of cell towers for improved network connectivity in parts of South Africa.",
+    image: "./mobile.gif",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Data Gathering",
+  "Data Management",
+  "Analytical Programming",
+  "Data Visualisation",
+  "Machine Learning",
+  "AWS Cloud Computing",
+  "Web App Development",
+  "Model Deployment",
+];
